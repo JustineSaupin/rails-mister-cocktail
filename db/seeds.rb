2af029be9d8d4cefd7086f4756eacafb70ba78b1
Justine Saupin <sau.justine@gmail.com>
2021-02-25 16:23:19 +0100

    controller cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+
+
+first = Cocktail.create(name:"1st Cocktail")
+
+Dose.create(description:"2gr", cocktail_id: first.id, ingredient_id: ice.id)
+Dose.create(description:"2cl", cocktail_id: first.id, ingredient_id: lemon.id)
