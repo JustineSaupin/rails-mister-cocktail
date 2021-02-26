9a3cbad45b72cd5b8384ac3fc192541a72692482
Justine Saupin <sau.justine@gmail.com>
2021-02-26 12:36:40 +0100

    Setup Clever Cloud config files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Dose.destroy_all
-Ingredient.destroy_all
-Cocktail.destroy_all
+
+#Dose.destroy_all
+#Ingredient.destroy_all
+#Cocktail.destroy_all
 
 lemon = Ingredient.create(name: "lemon")
 ice = Ingredient.create(name: "ice")
 mint = Ingredient.create(name: "mint leaves")
+white_rum = Ingredient.create(name: "white rum")
+brown_rum = Ingredient.create(name: "brown rum")
+spiced_rum = Ingredient.create(name: "spice rum")
+shweps = Ingredient.create(name: "shweps")
+sugar = Ingredient.create(name: "sugar")
+spices = Ingredient.create(name: "mixed spices")
+juice = Ingredient.create(name: "juice")
+deco = Ingredient.create(name: "lovely decoration")
 
 
-first = Cocktail.create(name:"1st Cocktail")
+#first = Cocktail.create(name:"1st Cocktail")
 
-Dose.create(description:"2gr", cocktail_id: first.id, ingredient_id: ice.id)
-Dose.create(description:"2cl", cocktail_id: first.id, ingredient_id: lemon.id)
+#Dose.create(description:"2gr", cocktail_id: first.id, ingredient_id: ice.id)
+#Dose.create(description:"2cl", cocktail_id: first.id, ingredient_id: lemon.id)
